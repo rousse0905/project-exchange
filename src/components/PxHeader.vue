10d3756512ab36a1b8566bd36627e916d12fc850
Rosa <rousse.0905@gmail.com>
2022-05-25 21:40:01 -0500

    Project Vue

---
+++ src/components/PxHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="shadow w-screen">
+    <nav>
+      <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <px-icon />
+          <router-link
+            class="font-semibold text-xl tracking-tight"
+            :to="{ name: 'home' }"
+            >PlatziExchange
+          </router-link>
+        </div>
+        <div
+          class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
+        >
+          <div class="text-sm lg:flex-grow">
+            <router-link 
+            v-for="l in links"
+            :key="l.title"
+            :to="l.to"
+            class="block mt-5 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" >
+             {{ l.title }} </router-link>
+          </div>
+        </div>
+      </nav>
+    </nav>
+  </header>
+</template>
+
+<script>
+import PxIcon from '@/components/PxIcon'
+
+export default {
+  name: 'PxHeader',
+  components: {
+    PxIcon,
+  },
+  props:{
+    links:{
+      type : Array,
+      default: () => []
+    }
+  }
+}
+</script>
